5a747b00b1277f70b3268b09261e872546e43256
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-09-26 15:31:57 -0300

    Update web_scraping.py
    Organizando os dados (Nome do jogo e preço) em dicionarios que serao organizados dentro de uma lista.

--- web_scraping.py
+++ web_scraping.py
@@ -6,60 +6,56 @@ import os
 
 def extrair():
   url = "https://store.epicgames.com/pt-BR/collection/top-sellers"
-  headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-  }
 
+  # Inicializa um novo serviço para o ChromeDriver
   service = Service()
+  # Permite definir opções para o navegador Chrome,
   options = webdriver.ChromeOptions()
 
+  #Cria uma nova instancia no navegador usando o serviço e as opções definidas anteriormente.
   driver = webdriver.Chrome(service = service, options = options)
-
   driver.get(url)
   
   sleep(3)
   os.system('cls')
   print("Carregando...")
-  element_text = driver.find_elements(By.CLASS_NAME, 'css-g3jcms')
+  lista_jogos = driver.find_elements(By.CLASS_NAME, 'css-g3jcms')
+  
   lst = []
-
-  for i in element_text:
-    lst.append(i.text)
-    lst.append("\n")
-
+  for jogo in lista_jogos:
+    j = jogo.text
+    j = j.split("\n")
+    jogo_obj = transformar(j)
+    lst.append(jogo_obj)
+  
   driver.quit()
-  lst = retirar_quebra_linha(lst)
-  
   print("DADOS COLETADOS COM SUCESSO!")
-      
   return lst
   
-def retirar_quebra_linha(lst):
-  for i in  range(len(lst)):
-    lst[i] = lst[i].replace('\n', ' ')
-  return lst
+def transformar(jogo): 
+  jogo_obj = {}
+  jogo_obj["Nome do jogo"] = jogo[1]
+  jogo_obj["Preco"] = jogo[-1]
+      
+  return jogo_obj
 
 def mostrar_dados(lst):
   for i in lst:
-    print(i)
+    print("Nome do jogo: " + i["Nome do jogo"] + "\nValor do jogo: " + i["Preco"] + "\n")
+
 
 def main():
   while True:
     print("1- COLETAR DADOS" + "\n2- MOSTRAR DADOS" + "\n3- ORDENAR DADOS")
     opc = input("Informe a opção desejada: ")
-    
     if opc == "1":
       lst = extrair()
       os.system('cls')
       
     elif opc == "2":
-      
       mostrar_dados(lst)
+    
     else:
       break
-
-
-
-      
 
 main()
